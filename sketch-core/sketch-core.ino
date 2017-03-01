345c0b3e3cbb69fac6448d48a877293e0721af16
Jose Constela <jose@joseconstela.com>
2017-03-02 00:00:27 +0100

    JSON validation and debug

--- sketch-core/sketch-core.ino
+++ sketch-core/sketch-core.ino
@@ -1,12 +1,10 @@
-/***********************************************
-* tiempodesiembra.es
-* root controller 0.0.1
+/*********************************************
+* tiempodesiembra.es                         *
+* root controller 0.0.2                     *
 **********************************************/
 
-const char VERSION = {"0.0.2"};
-
-/***********************************************
-* Device setup
+/*********************************************
+* Device setup                               *
 **********************************************/
 
 /* Tiempo de siembra */
@@ -21,28 +19,32 @@ const char DEVICE_LINK_CRYPTO = {"960e4ce7-53e2-48ac-b7bb-30ef00f2d20d"};
 const int RF24_CE_PIN                         = 9;     // nRF24L01 ce pin
 const int RF24_CSN_PIN                        = 10;    // nRF24L01 csn pin
 
-/***********************************************
-*
-*
-*    Do NOT change anything from this point
-*
-*
+const bool DEBUG                              = true;
+const String DEBUG_MESSAGE = "{\"d\":\"8d1b208e-cadb-4767-b199-cda6cbed3b0d\",\"v\":\"0.0.2\",\"moi\":[359,\"lowl\"],\"tem\":[341,\"lowl\"]}";
+
+/*********************************************
+*                                            *
+*                                            *
+*    Do NOT change anything from this point  *
+*                                            *
+*                                            *
 **********************************************/
 
-/***********************************************
-* Libraries
+/*********************************************
+* Libraries                                  *
 **********************************************/
 
 // Vars
 #include <printf.h>
+#include <ArduinoJson.h>
 
 // Comm
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <RF24_config.h>
 
-/***********************************************
-* System definitions
+/*********************************************
+* System definitions                         *
 **********************************************/
 
 /* Loop */
@@ -56,40 +58,63 @@ const uint64_t pipe = 0xE8E8F0F0E1LL;
 // Received message
 String message;
 
-/***********************************************
-* Functions
+const char* VERSION = {"0.0.2"};
+
+/*********************************************
+* Functions                                  *
 **********************************************/
 
 void setup(void){
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(1,pipe);
-  radio.startListening();
-  Serial.println("Start receiving");
+    
+  Serial.begin(9600); //initialize serial monitor
+  Serial.println("Tiempo de siembra, child controller: " + String(VERSION));
+
+  if (!DEBUG) {
+    Serial.println("Radio begin");
+    radio.begin();
+    radio.openReadingPipe(1,pipe);
+    radio.startListening();
+  }
+  
 }
 
 void parseMessage() {
-  Serial.println(message);
+  Serial.println("-");
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(message);
+
+  if (root.success()) {
+    // Transmit JSON over wifi
+  }
+
   message = "";
 }
 
-void loop(void){
+void loop(void) {
 
-  if (radio.available())
-  {
-    char text[32] = {0};
-    radio.read(&text, sizeof(text));
-
-    if((String(text) != "X") == 1)
+  if (DEBUG) {
+    message = DEBUG_MESSAGE;
+    parseMessage();
+  }
+  else {
+    if (radio.available())
     {
-      message = String(message + text);
-    }
-    else
-    {
-      parseMessage();
+      char text[32] = {0};
+      radio.read(&text, sizeof(text));
+  
+      if((String(text) != "~") == 1)
+      {
+        message = String(message + text);
+      }
+      else
+      {
+        parseMessage();
+      }
     }
   }
 
-  delay(LOOP_DELAY);
+  
+
+  delay(10000);
 
 }
